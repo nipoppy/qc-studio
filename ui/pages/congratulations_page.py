2dcbe0b7009e0f74ce3decc3836b33954fb49e59
manonheff <manon.heffernan@gmail.com>
2026-06-13 15:48:58 +0200

    added label statistics table and session duration histogram

--- ui/pages/congratulations_page.py
+++ ui/pages/congratulations_page.py
@@ -5,6 +5,8 @@ from constants import MESSAGES, SUCCESS_MESSAGES, INFO_MESSAGES
 from managers.session_manager import SessionManager
 from utils.export import save_qc_results_to_csv
 from constants import QC_RATINGS
+import pandas as pd
+import altair as alt
 
 def show_congratulations_page(qc_task: str, out_dir: str, total_participants: int, drop_duplicates: bool) -> None:
 	"""Display the final congratulations page after QC is complete.
@@ -68,16 +70,40 @@ def _display_session_summary(rater_id: str, qc_task: str, record_list: list) -> 
 		st.subheader("QC Results Summary")
 		# Count final_qc values
 		if record_list:
+			num_reviewed = len(record_list)
 			final_qc_counts = {}
+			durations = []
+			labels = []
 			for record in record_list:
 				qc_value = record.final_qc				
 				if qc_value not in QC_RATINGS:
 					final_qc_counts["Unrated"] = final_qc_counts.get(qc_value, 0) + 1
+					labels.append("Unrated")
 				else:
 					final_qc_counts[qc_value] = final_qc_counts.get(qc_value, 0) + 1
-			
-			for qc_status, count in sorted(final_qc_counts.items()):
-				st.write(f"**{qc_status}:** {count}")
+					labels.append(qc_value)
+				durations.append(int(record.duration))
+			duration_dict = {"duration":durations, "qc_value":labels}
+			duration_df = pd.DataFrame(duration_dict, columns=['duration','qc_value'])
+
+			final_qc_df = pd.DataFrame.from_dict(final_qc_counts, orient='index', columns=['Count'])
+			final_qc_df['Percentage'] = final_qc_df['Count'].apply(lambda x : x/num_reviewed)
+			#table with count and percentage per qc_value
+			st.table(data=final_qc_df, border=True, width="stretch", height="content", hide_index=None, hide_header=None)
+			#stacked histogram of duration count
+			selection = alt.selection_point(fields=['qc_value'], bind='legend')
+			line_chart = alt.Chart(duration_df).encode(
+				alt.X('duration:Q', title='Duration (s)',axis=alt.Axis(tickMinStep=1), bin=alt.Bin(step=1)),
+				alt.Y('count()',).stack(True), # change True to None to get non-stacked histogram
+				alt.Color('qc_value:N').scale(scheme="observable10"),
+				opacity=alt.condition(selection, alt.value(1), alt.value(0.2)) #link opacity to the selection state
+			).add_params(
+    			selection,
+			).mark_bar(
+				opacity=1 # to adapt if layering histograms
+			)
+
+			st.altair_chart(line_chart)
 
 
 def _export_qc_results(rater_id: str, out_dir: str, record_list: list, drop_duplicates: bool) -> None:
